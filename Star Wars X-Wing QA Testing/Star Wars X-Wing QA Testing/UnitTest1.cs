99ffbde7eb8f2bbe3feaad8547b58c17f5e51fe4
Brandon <bld@pdx.edu>
2021-09-17 14:33:48 -0700

    added NuGet packages for testing

--- Star Wars X-Wing QA Testing/Star Wars X-Wing QA Testing/UnitTest1.cs
+++ Star Wars X-Wing QA Testing/Star Wars X-Wing QA Testing/UnitTest1.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Star_Wars_X_Wing_QA_Testing
 {
