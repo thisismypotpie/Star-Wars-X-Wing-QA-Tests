8167a18c13c429bcc87830933ec12c630da04775
Brandon <bld@pdx.edu>
2021-09-20 12:51:05 -0700

    added two integration tests

---
+++ Star Wars X-Wing QA Testing/Star Wars X-Wing QA Testing/Integration Testing/FreeplayIntegrationTests.cs
@@ -0,0 +1,206 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Star_Wars_X_Wing_QA_Testing
+{
+    class FreeplayIntegrationTests
+    {
+        IWebDriver driver;
+        int freeplayTeamNumber = 1;
+        List<int> rosterNumbersInUse = new List<int>();
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            UtilityFunctions.freeplaySetup(ref driver);
+        }
+
+        [Test]
+        //Happy Path Test
+        //Create a single team with a single ship.
+        public void CreateNewTeam()
+        {
+            //Main team screen
+            IWebElement okTeamCreationButton;
+            IWebElement newTeamButton = driver.FindElement(By.Id("new-team-button"));
+            IWebElement teamNameTextField;
+            newTeamButton.Click();
+            okTeamCreationButton = driver.FindElement(By.Id("ok-button"));
+            teamNameTextField = driver.FindElement(By.Id("team-name-input"));
+            teamNameTextField.SendKeys("Test Team " + freeplayTeamNumber);
+            okTeamCreationButton.Click();
+
+            //Ship selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("New Team Ship Selection", driver.Url));
+            
+            IList<IWebElement> factionList = driver.FindElements(By.ClassName("faction-option"));
+            IList<IWebElement> shipSizeList;
+            IList<IWebElement> shipList;
+            int element_counter = UtilityFunctions.getRandomNumber(0, factionList.Count);
+            factionList[element_counter].Click();
+            shipSizeList = driver.FindElements(By.ClassName("ship-size-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipSizeList.Count);
+            shipSizeList[element_counter].Click();
+            shipList = driver.FindElements(By.ClassName("ship-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipList.Count);
+            shipList[element_counter].Click();
+
+            //Pilot selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("New Team Pilot Selection", driver.Url));
+            IWebElement direction_button = null;
+            bool pilot_selected = false;
+            while(pilot_selected == false)
+            {
+                element_counter = UtilityFunctions.getRandomNumber(1, 2);
+                if (element_counter == 1)//Go forward
+                {
+                    direction_button = driver.FindElement(By.Id("next-btn"));
+                }
+                else if (element_counter == 2)//Go backward
+                {
+                    direction_button = driver.FindElement(By.Id("previous-btn"));
+                }
+                else
+                {
+                    Assert.Fail("Pilot Selection screen failed. Random number to determine foward and backward motion returned out of bounds.");
+                }
+                element_counter = UtilityFunctions.getRandomNumber(0, 50);
+                for(int i=0; i < element_counter;i++)
+                {
+                    direction_button.Click();
+                }
+                if(!driver.FindElement(By.Id("unavailable")).Displayed)
+                {
+                    pilot_selected = true;
+                }
+            }
+            driver.FindElement(By.Id("select-button")).Click();
+
+            //Upgrade selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("New Team Upgrade Selection", driver.Url));
+            int numberOfUpgrades = UtilityFunctions.getRandomNumber(0,12);
+            for(int i=0; i < numberOfUpgrades;i++)
+            {
+                driver.FindElement(By.Id("next-selection")).Click();
+
+                //Upgrade type selection screen
+                Assert.IsTrue(UtilityFunctions.assertPageValidation("New Team Upgrade Type Selection", driver.Url));
+                IList<IWebElement> upgrade_types = driver.FindElements(By.ClassName("type-clicker"));
+                upgrade_types[UtilityFunctions.getRandomNumber(0, upgrade_types.Count)].Click();
+
+                //Upgrade Options screen
+                Assert.IsTrue(UtilityFunctions.assertPageValidation("New Team Upgrade Options", driver.Url));
+                IList<IWebElement> upgrades = driver.FindElements(By.ClassName("upgrade"));
+                upgrades[UtilityFunctions.getRandomNumber(0, upgrades.Count)].Click();
+            }
+            driver.FindElement(By.Id("done-button")).Click();
+            int roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            while(rosterNumbersInUse.Contains(roster_number))
+            {
+                roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            }
+            driver.FindElement(By.Id("roster-number-input")).SendKeys(roster_number.ToString());
+            driver.FindElement(By.Id("ok-button")).Click();
+
+            //Back to team screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("Team Page", driver.Url));
+            Assert.IsTrue(driver.FindElements(By.ClassName("team-summary")).Count == 1);
+            freeplayTeamNumber = driver.FindElements(By.ClassName("team-summary")).Count + 1;
+        }
+
+        [Test]
+        //Happy Path Test
+        //Add a single ship to an already existing team.
+        public void addNewShipToExistingTeam()
+        {
+            UtilityFunctions.createFreeplayTeam(ref driver, ref freeplayTeamNumber, ref rosterNumbersInUse);
+            int team_count = driver.FindElements(By.ClassName("team-summary")).Count;
+            driver.FindElement(By.Id("Test Team 1")).Click();
+            driver.FindElement(By.Id("add-button")).Click();
+
+            //Ship selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("Add Ship Ship Selection",driver.Url));
+
+            IList<IWebElement> factionList = driver.FindElements(By.ClassName("faction-option"));
+            IList<IWebElement> shipSizeList;
+            IList<IWebElement> shipList;
+            int element_counter = UtilityFunctions.getRandomNumber(0, factionList.Count);
+            factionList[element_counter].Click();
+            shipSizeList = driver.FindElements(By.ClassName("ship-size-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipSizeList.Count);
+            shipSizeList[element_counter].Click();
+            shipList = driver.FindElements(By.ClassName("ship-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipList.Count);
+            shipList[element_counter].Click();
+
+            //Pilot selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("Add Ship Pilot Selection", driver.Url));
+            IWebElement direction_button = null;
+            bool pilot_selected = false;
+            while (pilot_selected == false)
+            {
+                element_counter = UtilityFunctions.getRandomNumber(1, 2);
+                if (element_counter == 1)//Go forward
+                {
+                    direction_button = driver.FindElement(By.Id("next-btn"));
+                }
+                else if (element_counter == 2)//Go backward
+                {
+                    direction_button = driver.FindElement(By.Id("previous-btn"));
+                }
+                else
+                {
+                    Assert.Fail("Pilot Selection screen failed. Random number to determine foward and backward motion returned out of bounds.");
+                }
+                element_counter = UtilityFunctions.getRandomNumber(0, 50);
+                for (int i = 0; i < element_counter; i++)
+                {
+                    direction_button.Click();
+                }
+                if (!driver.FindElement(By.Id("unavailable")).Displayed)
+                {
+                    pilot_selected = true;
+                }
+            }
+            driver.FindElement(By.Id("select-button")).Click();
+
+            //Upgrade selection screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("Add Ship Upgrade Selection", driver.Url));
+            int numberOfUpgrades = UtilityFunctions.getRandomNumber(0, 12);
+            for (int i = 0; i < numberOfUpgrades; i++)
+            {
+                driver.FindElement(By.Id("next-selection")).Click();
+
+                //Upgrade type selection screen
+                Assert.IsTrue(UtilityFunctions.assertPageValidation("Add Ship Upgrade Type Selection", driver.Url));
+                IList<IWebElement> upgrade_types = driver.FindElements(By.ClassName("type-clicker"));
+                upgrade_types[UtilityFunctions.getRandomNumber(0, upgrade_types.Count)].Click();
+
+                //Upgrade Options screen
+                Assert.IsTrue(UtilityFunctions.assertPageValidation("Add Ship Upgrade Otions Selection", driver.Url));
+                IList<IWebElement> upgrades = driver.FindElements(By.ClassName("upgrade"));
+                upgrades[UtilityFunctions.getRandomNumber(0, upgrades.Count)].Click();
+            }
+            driver.FindElement(By.Id("done-button")).Click();
+            int roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            while (rosterNumbersInUse.Contains(roster_number))
+            {
+                roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            }
+            driver.FindElement(By.Id("roster-number-input")).SendKeys(roster_number.ToString());
+            driver.FindElement(By.Id("ok-button")).Click();
+
+            //Back to team screen
+            Assert.IsTrue(UtilityFunctions.assertPageValidation("Team Page", driver.Url));
+            Assert.IsTrue(driver.FindElements(By.ClassName("team-summary")).Count == team_count);
+            freeplayTeamNumber = driver.FindElements(By.ClassName("team-summary")).Count + 1;
+        }
+    }
+}
--- Star Wars X-Wing QA Testing/Star Wars X-Wing QA Testing/UtilityFunctions.cs
+++ Star Wars X-Wing QA Testing/Star Wars X-Wing QA Testing/UtilityFunctions.cs
@@ -45,5 +45,143 @@ namespace Star_Wars_X_Wing_QA_Testing
             Random rnd = new Random();
             return rnd.Next(low, high);
         }
+
+        public static void createFreeplayTeam(ref IWebDriver driver, ref int freeplayTeamNumber, ref List<int> rosterNumbersInUse)
+        {
+            //Main team screen
+            IWebElement okTeamCreationButton;
+            IWebElement newTeamButton = driver.FindElement(By.Id("new-team-button"));
+            IWebElement teamNameTextField;
+            newTeamButton.Click();
+            okTeamCreationButton = driver.FindElement(By.Id("ok-button"));
+            teamNameTextField = driver.FindElement(By.Id("team-name-input"));
+            teamNameTextField.SendKeys("Test Team " + freeplayTeamNumber);
+            okTeamCreationButton.Click();
+
+            //Ship selection screen
+            IList<IWebElement> factionList = driver.FindElements(By.ClassName("faction-option"));
+            IList<IWebElement> shipSizeList;
+            IList<IWebElement> shipList;
+            int element_counter = UtilityFunctions.getRandomNumber(0, factionList.Count);
+            factionList[element_counter].Click();
+            shipSizeList = driver.FindElements(By.ClassName("ship-size-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipSizeList.Count);
+            shipSizeList[element_counter].Click();
+            shipList = driver.FindElements(By.ClassName("ship-option"));
+            element_counter = UtilityFunctions.getRandomNumber(0, shipList.Count);
+            shipList[element_counter].Click();
+
+            //Pilot selection screen
+            IWebElement direction_button = null;
+            bool pilot_selected = false;
+            while (pilot_selected == false)
+            {
+                element_counter = UtilityFunctions.getRandomNumber(1, 2);
+                if (element_counter == 1)//Go forward
+                {
+                    direction_button = driver.FindElement(By.Id("next-btn"));
+                }
+                else if (element_counter == 2)//Go backward
+                {
+                    direction_button = driver.FindElement(By.Id("previous-btn"));
+                }
+                else
+                {
+                    Assert.Fail("Pilot Selection screen failed. Random number to determine foward and backward motion returned out of bounds.");
+                }
+                element_counter = UtilityFunctions.getRandomNumber(0, 50);
+                for (int i = 0; i < element_counter; i++)
+                {
+                    direction_button.Click();
+                }
+                if (!driver.FindElement(By.Id("unavailable")).Displayed)
+                {
+                    pilot_selected = true;
+                }
+            }
+            driver.FindElement(By.Id("select-button")).Click();
+
+            //Upgrade selection screen
+            int numberOfUpgrades = UtilityFunctions.getRandomNumber(0, 12);
+            for (int i = 0; i < numberOfUpgrades; i++)
+            {
+                driver.FindElement(By.Id("next-selection")).Click();
+
+                //Upgrade type selection screen
+                IList<IWebElement> upgrade_types = driver.FindElements(By.ClassName("type-clicker"));
+                upgrade_types[UtilityFunctions.getRandomNumber(0, upgrade_types.Count)].Click();
+
+                //Upgrade Options screen
+                IList<IWebElement> upgrades = driver.FindElements(By.ClassName("upgrade"));
+                upgrades[UtilityFunctions.getRandomNumber(0, upgrades.Count)].Click();
+            }
+            driver.FindElement(By.Id("done-button")).Click();
+            int roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            while (rosterNumbersInUse.Contains(roster_number))
+            {
+                roster_number = UtilityFunctions.getRandomNumber(1, 999);
+            }
+            driver.FindElement(By.Id("roster-number-input")).SendKeys(roster_number.ToString());
+            driver.FindElement(By.Id("ok-button")).Click();
+            freeplayTeamNumber = driver.FindElements(By.ClassName("team-summary")).Count + 1;
+        }
+
+        public static void addShipToExistingTeam()
+        {
+
+        }
+
+        public static bool assertPageValidation(string page, string url)
+        {
+            if(page == "Main Page" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Title-Screen(Main%20Menu)/index.html")
+            {
+                return true;
+            }
+            else if(page == "Team Page" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Team-Screen/Team-Screen.html")
+            {
+                return true;
+            }
+            else if(page == "New Team Ship Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Selection-Screen/Selection-Screen.html")
+            {
+                return true;
+            }
+            else if(page == "New Team Pilot Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Pilot-Screen/Pilot-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "New Team Upgrade Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Upgrade-Screen/Upgrade-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "New Team Upgrade Type Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Upgrade-Screen/Upgrade-Type-Selection-Screen/upgrade-type-selection-screen.html")
+            {
+                return true;
+            }
+            else if (page == "New Team Upgrade Options" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Upgrade-Screen/Upgrade-Selection-Screen/Upgrade-Selection-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "Add Ship Ship Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Add-New-Ship-Screens/Selection-Screen/New-Ship-Selection-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "Add Ship Pilot Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Add-New-Ship-Screens/Pilot-Screen/New-Ship-Pilot-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "Add Ship Upgrade Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Add-New-Ship-Screens/Upgrade-Screen/New-Ship-Upgrade-Screen.html")
+            {
+                return true;
+            }
+            else if (page == "Add Ship Upgrade Type Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Add-New-Ship-Screens/Upgrade-Screen/Upgrade-Type-Selection-Screen/upgrade-type-selection-screen.html")
+            {
+                return true;
+            }
+            else if (page == "Add Ship Upgrade Otions Selection" && url == "file:///C:/Users/Brandon%20Danielski/Documents/FullStackWebClass/Final-Project/Final-Project-Front-End/Front-End-Files/Add-New-Ship-Screens/Upgrade-Screen/Upgrade-Selection-Screen/Upgrade-Selection-Screen.html")
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
